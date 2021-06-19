77a27bc63d428e49ef2c5cf8b7f1f9835f1cb6fd
Leon Yao <leon.yao117@gmail.com>
2021-06-19 16:54:34 +0100

    enable https at launch

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const instance = axios.create({
   // baseURL: "http://157.230.214.200:3001",
-  baseURL: "http://localhost:3000",
+  baseURL: "https://localhost:3000",
   withCredentials: true,
 });
 

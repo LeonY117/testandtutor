00af67493fe99f427b332dce0bda334df0d152ce
Leon Yao <leon.yao117@gmail.com>
2021-10-21 00:37:06 +0100

    remove dummy data

--- src/containers/User/User.js
+++ src/containers/User/User.js
@@ -17,85 +17,12 @@ class User extends Component {
       subject: "IB Mathematics SL",
       examDate: "19th June",
       averageGrade: 6,
-      subTopics: {
-        "Functions and Equations": {
-          "Function Basics(Domain, Range, Inverse)": 5,
-          "Linear Functions": 6,
-          "Composite Functions": 5,
-          "Quadratic Functions": 5,
-          "Reciprocal and Rational Functions": 4,
-          "Logarithmic and Exponential Functions": 5,
-          "Transformation of Graphs": 6,
-        },
-        Algebra: {
-          "Arithmetic Progression": 5,
-          "Compound Interest": 6,
-          "Binomial Theorem": 5,
-          "Geometric Progression": 5,
-          Exponents: 4,
-          Logarithms: 5,
-        },
-        Calculus: {
-          "Chain Rule": 5,
-          "Derivativies of Standard Functions": 6,
-          "Integration by Substitution": 5,
-          Kinematics: 5,
-          "Product Rule": 4,
-          "Indefinite Integration": 5,
-          "Areas of Regions enclosed by Curves": 6,
-          "Definite Integrals": 6,
-          "Minimum/Maximum/Inflection points": 4,
-          Optimisation: 5,
-          "Quotient Rule": 4,
-          "Tangents and Normals at a given point": 7,
-          "Second Derivative (Concavity)": 5,
-        },
-        "Geometry and Trigonometry": {
-          "Distances and Midway Points": 5,
-          "Sine and Cosine Theorems": 6,
-          "Pythagorean Identity": 5,
-          "Double Angle Identity": 5,
-          "Trigonometric Equations": 4,
-          "Unit Circle": 5,
-          "Circle Geometry": 6,
-          "Volumes, Areas of 3D solids": 7,
-          "Area of a Triangle": 5,
-          "Circular Functions and Transformation": 6,
-        },
-        Statistics: {
-          "Probability Basics(Definition, Complementary Events)": 5,
-          "Mean, Median, Mode": 6,
-          "Box-and-Whisker Plots": 5,
-          "Quartiles and Interquartile range": 5,
-          "Venn Diagrams": 3,
-          "Binomial Distribution": 5,
-          "Independent Events": 6,
-          "Cumulative Frequency": 7,
-          "Normal Distribution": 5,
-          "Conditional Probabilities": 6,
-          Histograms: 6,
-          "Linear Regression": 6,
-          "Scatter Diagrams and Line of Best Fit": 6,
-          "Linear Correlation and Correlation Coefficient": 6,
-        },
-      },
+      subTopics: {},
       selectedTopic: "Functions and Equations",
-      topics: {
-        "Functions and Equations": 4,
-        Algebra: 3,
-        "Geometry and Trigonometry": 7,
-        Calculus: 5,
-        Statistics: 7,
-      },
-      suggestedTopics: {
-        "Scatter Diagrams and Line of Best Fit": 6,
-        "Normal Distribution": 5,
-        "Trigonometric Equations": 3,
-        "Reciprocal and Rational Functions": 4,
-        Exponents: 4,
-      },
-      recentTests: ["14587", "24567", "92840"],
-      scoreHistory: [50, 51, 68, 72, 80],
+      topics: {},
+      suggestedTopics: {},
+      recentTests: [],
+      scoreHistory: [],
     },
   };
 
@@ -132,6 +59,8 @@ class User extends Component {
           this.setState({ redirect: true });
         } else {
           const responseData = response.data.data;
+          console.log("from user profile:");
+          console.log(responseData);
           const dataCopy = { ...this.state.data };
 
           const topics = {};
@@ -160,6 +89,9 @@ class User extends Component {
           dataCopy.subTopics = subtopics;
           dataCopy.suggestedTopics = suggestedTopics;
 
+          console.log("forming this object from response:");
+          console.log(dataCopy);
+
           this.setState({ loading: false, data: dataCopy });
         }
       })
@@ -167,7 +99,7 @@ class User extends Component {
         // console.log("error!");
         console.log("cannot get user info ");
         this.setState({ redirect: true });
-        this.props.expired()
+        this.props.expired();
       });
   }
   render() {

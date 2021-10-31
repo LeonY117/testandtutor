eb00a4832872ccfd137865fc6b013fd0c7d6e47a
Leon Yao <leon.yao117@gmail.com>
2021-10-31 22:40:19 +0000

    refactor with es6 and major UI updates

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,106 +1,140 @@
-import React, { Component } from "react";
+import React, { Component, useEffect, useState } from "react";
+
+import Content from "hoc/Content/Content";
 import Input from "components/UI/Input/Input";
-import Content from "../../hoc/Content/Content";
-import Card from "../../components/UI/Card/Card";
-import Loading from "../../components/Loading/Loading";
-import Button from "../../components/UI/Button/Button";
+import Card from "components/UI/Card/Card";
+import Button from "components/UI/Button/Button";
+import Loading from "components/Loading/Loading";
 import classes from "./Login.module.css";
-import axios from "../../stores/axios";
+import axios from "stores/axios";
+import { Link } from "react-router-dom";
 
-class login extends Component {
-  state = {
-    username: null,
-    password: null,
-    warning: false,
-    errorMessage: null,
-    showPassword: false,
-    userID: null,
-    accessToken: null,
-    loading: false,
-  };
+const validateEmail = (email) => {
+  var re = /\S+@\S+\.\S+/;
+  return re.test(email);
+};
 
-  usernameInputChangedHandler = (event) => {
-    this.setState({ username: event.target.value });
-  };
-  passwordInputChangedHandler = (event) => {
-    this.setState({ password: event.target.value });
-  };
-  showPasswordHandler = () => {
-    console.log("clicked");
-    this.setState((prevState) => {
-      return { showPassword: !prevState.showPassword };
-    });
+const Login = (props) => {
+  const [username, setUsername] = useState("");
+  const [usernameWarning, setUsernameWarning] = useState(null);
+  const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  const passwordInputType = showPassword ? "text" : "password";
+
+  const usernameChangedHandler = (event) => {
+    setUsername(event.target.value);
   };
 
-  buttonClickedHandler = () => {
+  const passwordChangedHandler = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const showPasswordChangedHandler = () => {
+    setShowPassword((prevState) => !prevState);
+  };
+
+  const forgotPasswordHandler = () => {
+    return;
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    if (!validateEmail(username)) {
+      setUsernameWarning("please enter valid email");
+      return;
+    }
     const loginData = {
-      data: { email: this.state.username, password: this.state.password },
+      data: { email: username, password: password },
     };
-    this.setState({ loading: true });
+    setIsLoading(true);
     axios
-      .post("/auth/login", loginData, { withCredentials: true })
+      .post("/auth/login", loginData)
       .then((response) => {
+        console.log(response);
         const data = response.data;
-        // console.log(data)
         if (data.hasOwnProperty("errors")) {
-          this.setState({
-            warning: true,
-            errorMessage: data.errors[0].source.detail,
-            loading: false,
-          });
+          setErrorMessage(data.errors[0].source.detail);
         } else {
-          this.props.loginSuccessHandler(data.data.userId);
+          props.loginSuccessHandler();
         }
+        setIsLoading(false);
+      })
+      .catch(() => {
+        console.log("something went wrong.");
+        setIsLoading(false);
       });
-    //TODO catch own errors
   };
 
-  componentDidMount() {
+  useEffect(() => {
     window.scrollTo(0, 0);
-  }
+  }, []);
 
-  render() {
-    let passwordInputType = "password";
-    if (this.state.showPassword) {
-      passwordInputType = "text";
-    }
-
-    let warningMessage = null;
-    if (this.state.warning) {
-      warningMessage = (
-        <p className={classes.Warning}>{this.state.errorMessage}</p>
-      );
-    }
-    return (
-      <div className={classes.Login}>
-        <Content>
-          {this.state.loading ? <Loading /> : null}
-          <div className={classes.CardWrapper}>
-            <Card>
-              <h1>Log in </h1>
-              <div className={classes.InputFields}>
-                <p>Username: </p>
-                <Input type="text" changed={this.usernameInputChangedHandler} />
-                <p>Password:</p>
+  const loginInterface = (
+    <div className={classes.cardWrapper}>
+      <Card>
+        <div className={classes.login}>
+          <h1>Log in</h1>
+          <form onSubmit={submitHandler}>
+            <div className={classes.inputs}>
+              <Input
+                type="text"
+                placeholder={"Email"}
+                changed={usernameChangedHandler}
+                warning={usernameWarning}
+                autoFocus={true}
+              />
+              <div className={classes.passwordInput}>
                 <Input
                   type={passwordInputType}
-                  changed={this.passwordInputChangedHandler}
+                  placeholder={"Password"}
+                  changed={passwordChangedHandler}
                 />
-                <div className={classes.ShowPassword}>
-                  <input type="checkbox" onClick={this.showPasswordHandler} />
-                  <p>Show password</p>
-                </div>
+                <span>
+                  <p
+                    className={classes.showPasswordToggler}
+                    onClick={showPasswordChangedHandler}
+                  >
+                    {"Show"}
+                  </p>
+                </span>
               </div>
-              {warningMessage}
-              <Button round clicked={this.buttonClickedHandler}>
-                Log in
+              {errorMessage && (
+                <p className={classes.warning}>{errorMessage}</p>
+              )}
+            </div>
+            <div className={classes.buttonWrapper}>
+              <Button round clicked={submitHandler}>
+                {"Continue"}
               </Button>
-            </Card>
-          </div>
-        </Content>
-      </div>
-    );
-  }
-}
+            </div>
+          </form>
+          <Link>
+            <p
+              className={classes.forgotPassword}
+              onClick={forgotPasswordHandler}
+            >
+              {"forgot password?"}
+            </p>
+          </Link>
+          <p className={classes.signupPrompter}>
+            {"First time here? "}
+            <Link to="/signup">
+              <p className={classes.signupLink}>Sign up</p>
+            </Link>
+          </p>
+        </div>
+      </Card>
+    </div>
+  );
 
-export default login;
+  return (
+    <Content>
+      {isLoading && <Loading />}
+      {!isLoading && loginInterface}
+    </Content>
+  );
+};
+export default Login;

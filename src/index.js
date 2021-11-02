079d8ec159e89b144f4f06522cd4e4691071990b
Leon Yao <leon.yao117@gmail.com>
2021-11-02 15:20:57 +0000

    add auth context provider to wrap App component

--- src/index.js
+++ src/index.js
@@ -1,22 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
+
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+import { AuthContextProvider } from "./store/auth-context";
 import { BrowserRouter } from "react-router-dom";
 
-const app = (
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-);
-
 ReactDOM.render(
-  <React.StrictMode>{app}</React.StrictMode>,
+  <BrowserRouter>
+    <AuthContextProvider>
+      <App />
+    </AuthContextProvider>
+  </BrowserRouter>,
   document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();

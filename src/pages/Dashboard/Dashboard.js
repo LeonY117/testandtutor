b32cf39b619b0ac682bcf580ae13e77b36152b76
Leon Yao <leon.yao117@gmail.com>
2021-11-02 15:21:39 +0000

    change component from class based to function based

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,149 +1,128 @@
-import React, { Component } from "react";
-import Loading from "../../components/Loading/Loading";
-import UserProfile from "../../components/UserProfile/UserProfile";
+import React, { useContext, useEffect, useState } from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
 
-import axios from "../../stores/axios";
+import Loading from "components/Loading/Loading";
+import UserProfile from "components/UserProfile/UserProfile";
 
-class Dashboard extends Component {
-  state = {
-    redirect: false,
-    loading: true,
-    userId: "1234",
+import axios from "store/axios";
+import AuthContext from "store/auth-context";
+
+const Dashboard = (props) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [userData, setUserData] = useState({
+    username: "Student",
+    subject: "IB Mathematics SL",
+    examDate: "19th June",
+    averageGrade: 0,
+    topics: {},
+    subTopics: {},
     selectedTopic: "Functions and Equations",
-    data: {
-      username: "Student",
-      subject: "IB Mathematics SL",
-      examDate: "19th June",
-      averageGrade: 6,
-      subTopics: {},
-      selectedTopic: "Functions and Equations",
-      topics: {},
-      suggestedTopics: {},
-      recentTests: [],
-      scoreHistory: [50, 51, 68, 72, 80],
-    },
+    suggestedTopics: {},
+  });
+
+  const authCtx = useContext(AuthContext);
+
+  const selectChangedHandler = (event) => {
+    setUserData((prevState) => {
+      return { ...prevState, selectedTopic: event.target.value };
+    });
   };
 
-  selectChangedHandler = (event) => {
-    const dataCopy = { ...this.state.data };
-    dataCopy["selectedTopic"] = event.target.value;
-    this.setState({ data: dataCopy });
+  const takeTestButtonClickedHandler = () => {
+    props.history.push(props.match.url + "/test");
   };
 
-  takeTestButtonClickedHandler = () => {
-    this.props.history.push(this.props.match.url + "/test");
-  };
-
-  componentDidUpdate() {
-    if (!this.state.userId) {
-      this.props.expired();
-    }
-  }
-
-  componentDidMount() {
+  useEffect(() => {
     window.scrollTo(0, 0);
-    const data = {
-      data: { userId: this.state.userId },
-    };
-
-    // const headers = {
-    //   headers: { Authorization: `Bearer ${this.props.accessToken}` },
-    // };
     axios
       .get("/profiles/userprofile")
       .then((response) => {
         if (response.data.hasOwnProperty("errors")) {
           console.log(response.data);
           console.log("session expired");
-          this.setState({ redirect: true });
+          authCtx.logout();
         } else {
           const responseData = response.data.data;
           console.log("from user profile:");
           console.log(responseData);
-          const dataCopy = { ...this.state.data };
 
           const topics = {};
           const subtopics = {};
-          for (let i in responseData.topicScores) {
-            let topic = responseData.topicScores[i];
+          for (const topic of responseData.topicScores) {
             topics[topic.title] = parseInt(topic.score);
             subtopics[topic.title] = {};
-            for (let j in topic.subTopicScores) {
-              let sub = topic.subTopicScores[j];
-              subtopics[topic.title][sub.title] = parseInt(sub.score);
+            for (const subtopic of topic.subTopicScores) {
+              subtopics[topic.title][subtopic.title] = parseInt(subtopic.score);
             }
           }
 
           const suggestedTopics = {};
-          for (let i in responseData.suggestSubtopics) {
-            let sub = responseData.suggestSubtopics[i];
-            suggestedTopics[sub.title] = parseInt(sub.score);
+          for (const suggestedSubtopic of responseData.suggestSubtopics) {
+            suggestedTopics[suggestedSubtopic.title] = parseInt(
+              suggestedSubtopic.score
+            );
           }
 
-          dataCopy.username = responseData.first_name;
-          dataCopy.subject = responseData.curriculum;
-
-          // Not getting these from backend yet
-          dataCopy.topics = topics;
-          dataCopy.subTopics = subtopics;
-          dataCopy.suggestedTopics = suggestedTopics;
-
-          console.log("forming this object from response:");
-          console.log(dataCopy);
-
-          this.setState({ loading: false, data: dataCopy });
+          setIsLoading(false);
+          setUserData((prevState) => {
+            return {
+              ...prevState,
+              username: responseData.first_name,
+              subject: responseData.curriculum,
+              topics: topics,
+              subTopics: subtopics,
+              suggestedTopics: suggestedTopics,
+            };
+          });
         }
       })
       .catch((error) => {
-        // console.log(error);
+        console.log(error);
         console.log("cannot get user info ");
-        this.setState({ redirect: true });
-        this.props.expired();
+        authCtx.logout();
       });
-  }
-  render() {
-    const selectedTopicBreakdown = {
-      name: this.state.data.selectedTopic,
-      grade: this.state.data.topics[this.state.data.selectedTopic],
-      breakdown: this.state.data.subTopics[this.state.data.selectedTopic],
-    };
+  }, [authCtx]);
 
-    let userProfile = <Loading />;
+  const selectedTopicBreakdown = {
+    name: userData.selectedTopic,
+    grade: userData.topics[userData.selectedTopic],
+    breakdown: userData.subTopics[userData.selectedTopic],
+  };
 
-    if (this.state.loading === false) {
-      userProfile = (
-        <Switch>
-          <Route
-            path={this.props.match.url + "/profile"}
-            render={(props) => (
-              <UserProfile
-                {...props}
-                username={this.state.data.username}
-                subject={this.state.data.subject}
-                examDate={this.state.data.examDate}
-                averageGrade={this.state.data.averageGrade}
-                testButtonClicked={this.takeTestButtonClickedHandler}
-                topics={this.state.data.topics}
-                selectedTopicBreakdown={selectedTopicBreakdown}
-                selectChangedHandler={this.selectChangedHandler}
-                scoreHistory={this.state.data.scoreHistory}
-                suggestedTopics={this.state.data.suggestedTopics}
-              />
-            )}
+  const userProfile = (
+    <Switch>
+      <Route
+        path={props.match.url + "/profile"}
+        render={(props) => (
+          <UserProfile
+            {...props}
+            username={userData.username}
+            subject={userData.subject}
+            examDate={userData.examDate}
+            averageGrade={userData.averageGrade}
+            testButtonClicked={takeTestButtonClickedHandler}
+            topics={userData.topics}
+            selectedTopicBreakdown={selectedTopicBreakdown}
+            selectChangedHandler={selectChangedHandler}
+            suggestedTopics={userData.suggestedTopics}
           />
-          <Redirect from="/user" exact to="/user/profile" />
-        </Switch>
-      );
-    }
+        )}
+      />
+      <Redirect from="/user" exact to="/user/profile" />
+    </Switch>
+  );
 
-    if (this.state.redirect === true) {
-      console.log("going to redirect back go login");
-      userProfile = <Redirect from="/user" to="/login" />;
-    }
+  // if (this.state.redirect === true) {
+  //   console.log("going to redirect back go login");
+  //   userProfile = <Redirect from="/user" to="/login" />;
+  // }
 
-    return <div>{userProfile}</div>;
-  }
-}
+  return (
+    <div>
+      {isLoading && <Loading />}
+      {!isLoading && userProfile}
+    </div>
+  );
+};
 
 export default Dashboard;

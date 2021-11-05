c1631bbaf61c4c4a2fa44f433ea29ce017ba7f10
Leon Yao <leon.yao117@gmail.com>
2021-11-05 19:27:10 +0000

    folder restructure

---
+++ src/hoc/Layout/Footer/FooterItems/FooterItems.js
@@ -0,0 +1,13 @@
+import React from "react";
+import classes from './FooterItems.module.css'
+
+const footerItems = () => {
+  return (
+    <div className={classes.FooterItems}>
+      <p>Contact us!</p>
+      <p>leon.yao117@gmail.com</p>
+    </div>
+  );
+};
+
+export default footerItems;
--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Aux from "../Aux";
-import Toolbar from "../../components/Navigation /Toolbar/Toolbar";
-import Footer from "../../components/Footer/Footer";
+import Toolbar from "./Navigation/Toolbar/Toolbar";
+import Footer from "./Footer/Footer";
 import classes from "./Layout.module.css";
 
 const layout = (props) => {

95865c60f8b9f7af2fd5ea819c4dcf63ee2a616a
Leon Yao <leon.yao117@gmail.com>
2021-11-08 21:07:55 +0000

    refactor dashboard to call two APIs with axios

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,27 +1,27 @@
 import React, { useContext, useEffect, useState } from "react";
+import axios from "axios";
 // import { Switch, Route, Redirect } from "react-router-dom";
 import classes from "./Dashboard.module.css";
 
 import Content from "hoc/Content/Content";
+import Card from "components/UI/Card/Card";
 import Loading from "components/Loading/Loading";
 import Overview from "./Overview/Overview";
 import TopicBreakdown from "./TopicBreakdown/TopicBreakdown";
+import TestList from "pages/SelectTest/TestList/TestList";
 
-import axios from "store/axios";
+import axiosInstance from "store/axios";
 import AuthContext from "store/auth-context";
 
 const Dashboard = (props) => {
   const [isLoading, setIsLoading] = useState(true);
   const [userData, setUserData] = useState({
-    username: "Student",
-    subject: "IB Mathematics SL",
-    examDate: "19th June",
-    averageGrade: 0,
     topics: {},
     subtopics: {},
     selectedTopic: "Functions and Equations",
     suggestedTopics: {},
   });
+  const [tests, setTests] = useState({});
 
   const authCtx = useContext(AuthContext);
 
@@ -29,50 +29,88 @@ const Dashboard = (props) => {
     props.history.push(props.match.url + "/test");
   };
 
+  const testClickedHandler = () => {
+    return;
+  };
+
   useEffect(() => {
     window.scrollTo(0, 0);
+    const profileRequest = axiosInstance.get("/profiles/userprofile");
+    const selectTestRequest = axiosInstance.get("/tests/available_tests");
     axios
-      .get("/profiles/userprofile")
-      .then((response) => {
-        if (response.data.hasOwnProperty("errors")) {
-          console.log(response.data);
-          console.log("session expired");
-          authCtx.logout();
-        } else {
-          const responseData = response.data.data;
-          console.log("from user profile:");
-          console.log(responseData);
+      .all([profileRequest, selectTestRequest])
+      .then(
+        axios.spread((...responses) => {
+          // process response from '/profiles/userprofile'
+          const profileResponse = responses[0];
+          if (profileResponse.data.hasOwnProperty("errors")) {
+            console.log(profileResponse.data);
+            console.log("session expired");
+            authCtx.logout();
+          } else {
+            const profileResponseData = profileResponse.data.data;
+            console.log("from user profile:");
+            console.log(profileResponseData);
 
-          const topics = {};
-          const subtopics = {};
-          for (const topic of responseData.topicScores) {
-            topics[topic.title] = parseInt(topic.score);
-            subtopics[topic.title] = {};
-            for (const subtopic of topic.subTopicScores) {
-              subtopics[topic.title][subtopic.title] = parseInt(subtopic.score);
+            const topics = {};
+            const subtopics = {};
+            for (const topic of profileResponseData.topicScores) {
+              topics[topic.title] = parseInt(topic.score);
+              subtopics[topic.title] = {};
+              for (const subtopic of topic.subTopicScores) {
+                subtopics[topic.title][subtopic.title] = parseInt(
+                  subtopic.score
+                );
+              }
             }
+
+            const suggestedTopics = {};
+            for (const suggestedSubtopic of profileResponseData.suggestSubtopics) {
+              suggestedTopics[suggestedSubtopic.title] = parseInt(
+                suggestedSubtopic.score
+              );
+            }
+
+            setUserData((prevState) => {
+              return {
+                ...prevState,
+                username: profileResponseData.first_name,
+                subject: profileResponseData.curriculum,
+                topics: topics,
+                subtopics: subtopics,
+                suggestedTopics: suggestedTopics,
+              };
+            });
           }
 
-          const suggestedTopics = {};
-          for (const suggestedSubtopic of responseData.suggestSubtopics) {
-            suggestedTopics[suggestedSubtopic.title] = parseInt(
-              suggestedSubtopic.score
-            );
-          }
+          // process response from '/test/available_tests'
+          const selectTestResponse = responses[1];
+          if (selectTestResponse.data.hasOwnProperty("errors")) {
+            console.log(selectTestResponse.data);
+            console.log("session expired");
+            authCtx.logout();
+          } else {
+            const selectTestResponseData = selectTestResponse.data.data;
+            console.log(selectTestResponseData);
+            const testsCopy = {};
 
-          setUserData((prevState) => {
-            return {
-              ...prevState,
-              username: responseData.first_name,
-              subject: responseData.curriculum,
-              topics: topics,
-              subtopics: subtopics,
-              suggestedTopics: suggestedTopics,
-            };
-          });
+            for (const test of selectTestResponseData) {
+              testsCopy[test.id] = {};
+              testsCopy[test.id]["name"] = test.title;
+              testsCopy[test.id]["status"] = test.last_attempt
+                ? "finished"
+                : "unfinished";
+              testsCopy[test.id]["length"] = test.duration;
+              testsCopy[test.id]["score"] = test.last_attempt
+                ? test.last_attempt.score
+                : null;
+              testsCopy[test.id]["totalScore"] = test.max_marks; //test.totalScore
+            }
+            setTests(testsCopy);
+          }
           setIsLoading(false);
-        }
-      })
+        })
+      )
       .catch((error) => {
         console.log(error);
         console.log("cannot get user info ");
@@ -80,50 +118,64 @@ const Dashboard = (props) => {
       });
   }, [authCtx]);
 
-  const userProfile = (
+  // TODO: Filter to 5 most recent tests
+  // or get recent tests from user/userprofile
+  const completeTestObj = {};
+  const incompleteTestObj = {};
+
+  for (const testId in tests) {
+    let test = tests[testId];
+    if (test.status === "finished") {
+      completeTestObj[testId] = test;
+    } else if (test.status === "unfinished") {
+      incompleteTestObj[testId] = test;
+    }
+  }
+
+  // TODO: potentially make this into a new component or combine
+  // with 'pages/SelectTest'
+  const testListCard = (
+    <Card>
+      <p className={classes.testTitle}>Completed Tests</p>
+      <TestList
+        tests={completeTestObj}
+        testSelectButtonClicked={testClickedHandler}
+      />
+      <p className={classes.testTitle} style={{ marginTop: "3rem" }}>
+        Incomplete Tests
+      </p>
+      <TestList
+        tests={incompleteTestObj}
+        testSelectButtonClicked={testClickedHandler}
+      />
+    </Card>
+  );
+
+  const dashboard = (
     <div>
       <Content>
         <div className={classes.userGreeting}>
           <h1>Hi there!</h1>
         </div>
-        <div className={classes.overviewWrapper}>
+        <section className={classes.overviewWrapper}>
           <Overview
             topics={userData.topics}
             suggestions={userData.suggestedTopics}
             testButtonClicked={takeTestButtonClickedHandler}
           />
-        </div>
-        <TopicBreakdown subtopics={userData.subtopics} />
+        </section>
+        <section className={classes.breakdownWrapper}>
+          <TopicBreakdown subtopics={userData.subtopics} />
+        </section>
+        <section className={classes.selectTestWrapper}>{testListCard}</section>
       </Content>
-      {/* <Route
-        path={props.match.url + "/profile"}
-        render={(props) => (
-          <UserProfile
-            {...props}
-            username={userData.username}
-            subject={userData.subject}
-            examDate={userData.examDate}
-            averageGrade={userData.averageGrade}
-            testButtonClicked={takeTestButtonClickedHandler}
-            topics={userData.topics}
-            selectedTopicBreakdown={selectedTopicBreakdown}
-            selectChangedHandler={selectChangedHandler}
-            suggestedTopics={userData.suggestedTopics}
-          />
-        )}
-      /> */}
       {/* <Redirect from="/user" exact to="/user/profile" /> */}
     </div>
   );
-
-  // if (this.state.redirect === true) {
-  //   console.log("going to redirect back go login");
-  //   userProfile = <Redirect from="/user" to="/login" />;
-  // }
   return (
     <div>
       {isLoading && <Loading />}
-      {!isLoading && userProfile}
+      {!isLoading && dashboard}
     </div>
   );
 };

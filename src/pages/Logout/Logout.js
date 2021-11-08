01004c7ee5fbd998ecf0e220cb272eb746b1c7c9
Leon Yao <leon.yao117@gmail.com>
2021-11-08 16:31:00 +0000

    Refactor component

--- src/pages/Logout/Logout.js
+++ src/pages/Logout/Logout.js
@@ -1,29 +1,29 @@
-import Cookies from "js-cookie";
 import React, { useEffect, useContext } from "react";
 import { Redirect } from "react-router-dom";
 
-import axios from "../../store/axios";
-import AuthContext from "../../store/auth-context";
-import Loading from "../../components/Loading/Loading";
+import axios from "store/axios";
+import AuthContext from "store/auth-context";
+import Loading from "components/Loading/Loading";
 
 const Logout = () => {
   const authCtx = useContext(AuthContext);
-  const userIsLoggedIn = authCtx.isLoggedIn;
   useEffect(() => {
     axios.post("auth/logout", {}).then((response) => {
       console.log(response);
       if (response.data.hasOwnProperty("error")) {
         console.log("error");
+        authCtx.logout();
       } else {
         authCtx.logout();
       }
     });
   });
-  let logout = <Loading />;
-  if (!userIsLoggedIn) {
-    logout = <Redirect from="/logout" to="/" />;
-  }
-  return <div>{logout}</div>;
+  return (
+    <div>
+      {authCtx.isLoggedIn && <Loading />}
+      {!authCtx.isLoggedIn && <Redirect from="/logout" to="/" />}
+    </div>
+  );
 };
 
 export default Logout;

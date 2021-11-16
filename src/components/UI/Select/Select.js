27a8744e708008fd4a907dc8d04959b077944ac0
Leon Yao <leon.yao117@gmail.com>
2021-11-16 16:56:35 +0000

    update select styling

--- src/components/UI/Select/Select.js
+++ src/components/UI/Select/Select.js
@@ -1,21 +1,29 @@
 import React from "react";
 import classes from "./Select.module.css";
 
-const customSelect = (props) => {
+const Select = (props) => {
+  let classArray = [classes.select];
+
+  if (props.round) {
+    classArray.push(classes.round);
+  }
+
   let options = null;
   if (props.options) {
-    options = props.options.map((op) => (
-      <option key={op} value={op}>
+    options = props.options.map((op, i) => (
+      <option className={classes.option} key={i} value={op}>
         {op}
       </option>
     ));
   }
   return (
-    <div className={classes.customSelect}>
-      <select onChange={props.changed}>{options}</select>
+    <div className={classes.selectWrapper}>
+      <select className={classArray.join(' ')} onChange={props.changed}>
+        {options}
+      </select>
       <span className={classes.customArrow}></span>
     </div>
   );
 };
 
-export default customSelect;
+export default Select;

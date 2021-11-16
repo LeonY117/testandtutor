0bc03c71e15abbdbd384369115eaded78e5ee046
Leon Yao <leon.yao117@gmail.com>
2021-11-16 17:31:54 +0000

    change clicked to onClick for a tag

--- src/hoc/Layout/Navigation/NavigationItems/NavigationItems.js
+++ src/hoc/Layout/Navigation/NavigationItems/NavigationItems.js
@@ -16,20 +16,12 @@ const navigationItems = (props) => {
   let nav = (
     <ul className={classes.navigationItems}>
       <li className={classes.navigationItem}>
-        <NavLink
-          to="/login"
-          className={classes.loginLink}
-          clicked={props.clicked}
-        >
+        <NavLink to="/login" className={classes.loginLink}>
           Sign in
         </NavLink>
       </li>
       <li className={classes.navigationItem}>
-        <NavLink
-          to="/signup"
-          className={classes.getStartedLink}
-          clicked={props.clicked}
-        >
+        <NavLink to="/signup" className={classes.getStartedLink}>
           Get started
         </NavLink>
       </li>

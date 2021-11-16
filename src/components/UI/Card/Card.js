6784894b2aa9ecc4d34516d78a5fea4f2f67cf1f
Leon Yao <leon.yao117@gmail.com>
2021-11-16 16:57:10 +0000

    add page wrapper styling to card

--- src/components/UI/Card/Card.js
+++ src/components/UI/Card/Card.js
@@ -1,8 +1,12 @@
 import classes from "./Card.module.css";
 import React from "react";
 
-const card = (props) => {
-  return <div className={classes.Card}>{props.children}</div>;
+const Card = (props) => {
+  let classArray = [classes.card];
+  if (props.pageWrapper) {
+    classArray.push(classes.pageWrapper);
+  }
+  return <div className={classArray.join(" ")}>{props.children}</div>;
 };
 
-export default card;
+export default Card;

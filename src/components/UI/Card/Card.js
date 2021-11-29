42686f6b2009f4994921f5d3a337840eac3f2292
Leon Yao <leon.yao117@gmail.com>
2021-11-29 16:33:53 +0000

    add shadow and round corner options

--- src/components/UI/Card/Card.js
+++ src/components/UI/Card/Card.js
@@ -6,6 +6,12 @@ const Card = (props) => {
   if (props.pageWrapper) {
     classArray.push(classes.pageWrapper);
   }
+  if (props.blurShadow) {
+    classArray.push(classes.blurShadow);
+  }
+  if (props.round) {
+    classArray.push(classes.round);
+  }
   return <div className={classArray.join(" ")}>{props.children}</div>;
 };
 

1fe1fea3e55e2cf1d7e859abcec3cd3907fa0d65
Leon Yao <leon.yao117@gmail.com>
2021-11-29 16:34:08 +0000

    update landing page

--- src/pages/Landing/Landing.js
+++ src/pages/Landing/Landing.js
@@ -1,124 +1,130 @@
 import React from "react";
-import Aux from "../../hoc/Aux";
-import Logo from "../../components/Logo/Logo";
+import { Link } from "react-router-dom";
 import classes from "./Landing.module.css";
-import Card from "../../components/UI/Card/Card";
 
-import image1 from "../../assets/images/personStudyingMath.jpg";
-import image2 from "../../assets/images/radar5.png";
-import image3 from "../../assets/images/suggestions.png";
-import image4 from "../../assets/images/distribution.png";
+import Logo from "components/Logo/Logo";
+import Card from "components/UI/Card/Card";
+import Button from "components/UI/Button/Button";
+import Question from "components/Test/Question/Question";
+import Content from "hoc/Content/Content";
 
-const landing = (props) => {
+import image1 from "assets/images/personStudyingMath.jpg";
+import dummyQuestionData from "./dummyQuestion.json";
+import Radar from "components/Radar/Radar";
+
+const dummyRadarData = {
+  "Functions and Equations": 6,
+  Algebra: 7,
+  "Geometry and Trigonometry": 5,
+  Calculus: 3,
+  Statistics: 4,
+};
+
+const Landing = () => {
   return (
-    <Aux>
-      <div className={classes.LandingBanner}>
-        <div className={classes.Logo}>
+    <React.Fragment>
+      <section className={classes.LandingBanner}>
+        <div className={classes.logoWrapper}>
           <Logo />
         </div>
         <h1>Personalised Study Planner for IB Mathematics</h1>
-        <a className={classes.ActionButton} href="/">
-          GET EXCLUSIVE ACCESS NOW
-        </a>
-      </div>
+        <Link to="/signup">
+          <Button size="large" round>
+            GET STARTED NOW
+          </Button>
+        </Link>
+      </section>
 
-      <div className={classes.LandingInfos}>
-        <div className={classes.LandingInfo}>
-          <div className={classes.Description}>
-            <h1>
-              Get a <strong>7</strong> in IB Mathematics
-            </h1>
-            <p>
-              Statistical models built specifically for IB Mathematics to
-              determine the optimal path for you to achieve the your best grades
-            </p>
-          </div>
-          <div className={classes.CardWrapper}>
-            <img
-              alt={"Illustration of a person studying maths"}
-              src={image1}
-              className={classes.Illustration}
-              draggable={false}
-            />
-          </div>
-        </div>
-
-        <div className={classes.LandingInfo}>
-          <div className={classes.CardWrapper}>
-            <Card>
-              <img
-                alt={
-                  "Illustration of a radar chart displaying relative strengths of a student"
-                }
-                src={image2}
-                className={classes.Illustration}
-                draggable={false}
-              />
-            </Card>
+      <section className={classes.LandingInfos}>
+        <Content>
+          <div className={classes.LandingInfo}>
+            <div className={classes.descriptionWrapper}>
+              <h1>
+                Get a <strong>7</strong> in IB Mathematics
+              </h1>
+              <p>
+                Statistical models built specifically for IB Mathematics to
+                determine the optimal path for you to achieve the your best
+                grades
+              </p>
+            </div>
+            <div className={classes.cardWidthControl}>
+              <div className={classes.cardWrapper}>
+                <Card blurShadow round>
+                  <img
+                    alt={"Illustration of a person studying maths"}
+                    src={image1}
+                    className={classes.Illustration}
+                    draggable={false}
+                  />
+                </Card>
+              </div>
+            </div>
           </div>
 
-          <div className={classes.Description}>
-            <h1>
-              <strong>Personalised</strong> <br />
-              Study Plans
-            </h1>
-            <p>
-              We provide you with personalised suggestions for topics to study
-              next, so that you can always focus on your biggest weaknesses.
-            </p>
+          <div className={classes.overflowControl}>
+            <div
+              className={[classes.LandingInfo, classes.LandingInfoFlipped].join(
+                " "
+              )}
+            >
+              <div className={classes.cardWidthControl}>
+                <div className={classes.cardWrapper}>
+                  <Card round blurShadow>
+                    {/* <img
+                  alt={
+                    "Illustration of a radar chart displaying relative strengths of a student"
+                  }
+                  src={image2}
+                  className={classes.Illustration}
+                  draggable={false}
+                /> */}
+                    <Radar topics={dummyRadarData} showText />
+                  </Card>
+                </div>
+              </div>
+              <div className={classes.descriptionWrapper}>
+                <h1>
+                  <strong>Personalised</strong> <br />
+                  Insights
+                </h1>
+                <p>
+                  Studying should be an iterative process. <br />
+                  <br /> Test and tutor objectively identifies your strengths
+                  and weaknesses, and helps you decide what to study next.
+                </p>
+              </div>
+            </div>
           </div>
-        </div>
 
-        <div className={classes.LandingInfo}>
-          <div className={classes.Description}>
-            <h1>
-              <strong>Insights</strong> into your strengths and weaknesses
-            </h1>
-            <p>
-              We track your long term performance from hundreds of labelled
-              questions, and provide you with insights into your relative
-              performance in each topic.
-            </p>
+          <div className={classes.LandingInfo}>
+            <div className={classes.descriptionWrapper}>
+              <h1>
+                <strong>Exam Style</strong> Questions
+              </h1>
+              <p>
+                Hundreds of exam-style questions and their solutions, filtered
+                by topic and subtopic.
+              </p>
+            </div>
+            <div className={classes.cardWidthControl}>
+              <div
+                className={[classes.cardWrapper, classes.questionCard].join(
+                  " "
+                )}
+              >
+                <Card round blurShadow>
+                  <div className={classes.questionWrapper}>
+                    <Question questionData={dummyQuestionData} />
+                  </div>
+                </Card>
+              </div>
+            </div>
           </div>
-          <div className={classes.CardWrapper} style={{ padding: "2rem" }}>
-            <Card>
-              <img
-                alt={"Illustration of a list of suggested topics"}
-                src={image3}
-                className={classes.Illustration}
-                draggable={false}
-              />
-            </Card>
-          </div>
-        </div>
-
-        <div className={classes.LandingInfo}>
-          <div className={classes.CardWrapper}>
-            <Card>
-              <img
-                alt={
-                  "Illustration of a probability distribution of a student's grade"
-                }
-                src={image4}
-                className={classes.Illustration}
-                draggable={false}
-              />
-            </Card>
-          </div>
-          <div className={classes.Description}>
-            <h1>
-              Track your <br />
-              <strong>predicted grade</strong>
-            </h1>
-            <p>
-              The statistical model uses your performances in each topic to
-              predict what grade you are most likely to get in your final exam.
-            </p>
-          </div>
-        </div>
-      </div>
-    </Aux>
+        </Content>
+      </section>
+    </React.Fragment>
   );
 };
 
-export default landing;
+export default Landing;

b13f970bbb163ba06b9605047bb8712e0480a8b2
Leon Yao <leon.yao117@gmail.com>
2021-12-02 19:43:16 +0000

    add redirect from signup

--- src/App.js
+++ src/App.js
@@ -25,6 +25,11 @@ function App() {
   if (userIsLoggedIn) {
     redirectFromLogin = <Redirect from="/login" to="/user" />;
   }
+  let redirectFromSignup = null;
+  
+  if (userIsLoggedIn) {
+    redirectFromSignup = <Redirect from="/signup" to="/user" />;
+  }
   let redirectFromUser = null;
   if (userIsLoggedIn === false) {
     redirectFromUser = <Redirect from="/user" to="/login" />;
@@ -33,10 +38,9 @@ function App() {
     <Layout mode={userIsLoggedIn ? "user" : "visitor"}>
       <Switch>
         {redirectFromUser}
+        {redirectFromSignup}
         {redirectFromLogin}
         <Route exact path="/" component={Landing} />
-        <Route exact path="/login" component={Login} />
-        <Route exact path="/signup" component={Signup} />
         <Route exact path="/user/test" component={SelectTest} />
         <Route exact path="/user/test/:id" component={TestPaper} />
         <Route exact path="/user/testSubmitted" component={TestSubmitConfirm} />
@@ -44,6 +48,8 @@ function App() {
         <Route exact path="/user/settings" component={Settings} />
         <Route exact path="/forgotPassword/:token" component={ForgotPassword} />
         <Route exact path="/logout" component={Logout} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/signup" component={Signup} />
         <Route path="/" component={NotFound} />
       </Switch>
     </Layout>

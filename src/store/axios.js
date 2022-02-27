24f4d558899850abb5e19c64182106eb4fd9cc9b
Leon Yao <leon.yao117@gmail.com>
2022-02-27 20:05:42 +0000

    add baseURL option

--- src/store/axios.js
+++ src/store/axios.js
@@ -3,6 +3,7 @@ import axios from "axios";
 const instance = axios.create({
   // baseURL: "http://157.230.214.200:3001",
   baseURL: "https://localhost:3000",
+  // baseURL: "https://testandtutor.com:3001",
   // baseURL: "https://ec2-18-133-161-49.eu-west-2.compute.amazonaws.com:3000",
   withCredentials: true,
 });

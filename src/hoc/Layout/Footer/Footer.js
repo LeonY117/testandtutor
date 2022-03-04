ba77205c7fd53c81478c9cde3043c486980f7092
Leon Yao <leon.yao117@gmail.com>
2022-03-04 17:11:30 +0000

    add bugReport button

--- src/hoc/Layout/Footer/Footer.js
+++ src/hoc/Layout/Footer/Footer.js
@@ -1,13 +1,32 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 import classes from "./Footer.module.css";
 
 import Logo from "components/Logo/Logo";
 import FooterItems from "./FooterItems/FooterItems";
+import Button from "components/UI/Button/Button";
 
 const Footer = (props) => {
+  const history = useHistory();
+  const reportButtonClickedHandler = () => {
+    history.push("/bugReport");
+  };
   let footer = null;
   if (props.mode === "user") {
-    footer = null;
+    footer = (
+      <div>
+        <div className={classes.buttonWrapper}>
+          <Button
+            tertiary
+            color="grey"
+            narrow
+            clicked={reportButtonClickedHandler}
+          >
+            Report an issue
+          </Button>
+        </div>
+      </div>
+    );
   } else if (props.mode === "visitor") {
     footer = (
       <div className={classes.Footer}>

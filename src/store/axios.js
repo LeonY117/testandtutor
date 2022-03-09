942b0b4e8447b4dba5e0bf49b9d4afbc031ce694
Leon Yao <leon.yao117@gmail.com>
2022-03-09 23:52:58 +0000

    use node env to determine backend URL

--- src/store/axios.js
+++ src/store/axios.js
@@ -1,10 +1,18 @@
 import axios from "axios";
 
+const environment = process.env.NODE_ENV;
+
+const URLMAPPING = {
+  development: "https://localhost:3000",
+  production: "https://testandtutor.com:3001",
+};
+
+// // baseURL: "http://157.230.214.200:3001",
+// baseURL: "https://localhost:3000",
+// // baseURL: "https://testandtutor.com:3001",
+// // baseURL: "https://ec2-18-133-161-49.eu-west-2.compute.amazonaws.com:3000"
 const instance = axios.create({
-  // baseURL: "http://157.230.214.200:3001",
-  baseURL: "https://localhost:3000",
-  // baseURL: "https://testandtutor.com:3001",
-  // baseURL: "https://ec2-18-133-161-49.eu-west-2.compute.amazonaws.com:3000",
+  baseURL: URLMAPPING[environment],
   withCredentials: true,
 });
 

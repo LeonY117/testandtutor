1990416a16707e6de340680186df01fad3f3dc4b
Leon Yao <leon.yao117@gmail.com>
2022-10-01 15:09:39 +0100

    update sorting method

--- src/components/TestLists/TestList/TestList.js
+++ src/components/TestLists/TestList/TestList.js
@@ -1,8 +1,8 @@
-import React from "react";
-import { Link, useHistory } from "react-router-dom";
-import classes from "./TestList.module.css";
+import React from 'react';
+import { Link, useHistory } from 'react-router-dom';
+import classes from './TestList.module.css';
 
-import Button from "components/UI/Button/Button";
+import Button from 'components/UI/Button/Button';
 
 const TestInfo = (props) => {
   return (
@@ -38,7 +38,7 @@ const TestList = (props) => {
 
   const testSelectButtonClicked = (id, status) => {
     // console.log(status);
-    if (status === "finished") {
+    if (status === 'finished') {
       alert("Retaking tests won't affect your profile");
       history.push(`/user/test/${id}`);
     } else {
@@ -48,29 +48,48 @@ const TestList = (props) => {
 
   if (props.tests) {
     let testEntries = Object.entries(props.tests);
+    console.log(testEntries)
     let sortedTests = testEntries.sort((a, b) => {
-      let x = a[1].name.toLowerCase();
-      let y = b[1].name.toLowerCase();
-      return x < y ? -1 : x > y ? 1 : 0;
+      // Assuming that the name of the test is something like Algebra Test 2
+      let item_a = a[1].name.split(' ');
+      let item_b = b[1].name.split(' ');
+      let name_a = item_a[0].toLowerCase();
+      let name_b = item_b[0].toLowerCase();
+      let num_a = parseInt(item_a[2]);
+      let num_b = parseInt(item_b[2]);
+      // first order by name, then order by number
+      if (name_a < name_b) {
+        return -1;
+      } else if (name_a > name_b) {
+        return 1;
+      } else {
+        if (num_a < num_b) {
+          return -1;
+        } else if (num_a > num_b) {
+          return 1;
+        } else {
+          return 0;
+        }
+      }
     });
     tests = sortedTests.map((testEntry) => {
       let testKey = testEntry[0];
       let test = testEntry[1];
       let status = test.status;
       let score = null;
-      let buttonInfo = "Take test";
-      let buttonColor = "orange";
-      if (status === "finished") {
-        score = test.totalScore + "/" + test.score;
-        buttonInfo = "Retake test";
-        buttonColor = "white";
+      let buttonInfo = 'Take test';
+      let buttonColor = 'orange';
+      if (status === 'finished') {
+        score = test.totalScore + '/' + test.score;
+        buttonInfo = 'Retake test';
+        buttonColor = 'white';
       }
       return (
         <TestInfo
           key={testKey}
           testId={testKey}
           name={test.name}
-          length={test.length + " min"}
+          length={test.length + ' min'}
           score={score}
           buttonInfo={buttonInfo}
           buttonColor={buttonColor}

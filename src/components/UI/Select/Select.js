ade7ac1375984dde5272f57c684ca648d69df336
Leon Yao <leon.yao117@gmail.com>
2022-10-01 15:09:57 +0100

    remove default Value

--- src/components/UI/Select/Select.js
+++ src/components/UI/Select/Select.js
@@ -1,18 +1,18 @@
-import React from "react";
-import classes from "./Select.module.css";
+import React from 'react';
+import classes from './Select.module.css';
 
 const Select = (props) => {
   let classArray = [classes.select];
   let widthControlClasses = [classes.selectWrapper];
-  let hideArrow = props.disabled ? "true" : "false";
+  let hideArrow = props.disabled ? 'true' : 'false';
   props.round && classArray.push(classes.round);
   props.narrow && widthControlClasses.push(classes.narrow);
 
-  if (props.size === "large") {
+  if (props.size === 'large') {
     classArray.push(classes.large);
-  } else if (props.size === "medium") {
+  } else if (props.size === 'medium') {
     classArray.push(classes.medium);
-  } else if (props.size === "small") {
+  } else if (props.size === 'small') {
     classArray.push(classes.small);
   }
 
@@ -31,20 +31,20 @@ const Select = (props) => {
     });
     if (props.defaultLabel) {
       options.push(
-        <option className={classes.option} key={-1} value={""} hidden>
+        <option className={classes.option} key={-1} value={''} hidden>
           {props.defaultLabel}
         </option>
       );
     }
   }
   return (
-    <div className={widthControlClasses.join(" ")}>
+    <div className={widthControlClasses.join(' ')}>
       <select
-        className={classArray.join(" ")}
+        className={classArray.join(' ')}
         onChange={props.changed}
         value={props.selected}
         disabled={props.disabled}
-        defaultValue={""}
+        // defaultValue={""}
       >
         {options}
       </select>
